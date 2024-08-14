74927672f85f8e5fceebfa5fea0d2edfcabceb0a
LiviaSobral <Liviaheloisa2005@gmail.com>
2024-08-14 20:56:20 -0300

    Atualizando

--- Aulas/Aula21/exer1/src/App.js
+++ Aulas/Aula21/exer1/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Calculadora from './componentes/compCalculadora';
+import Contador from './componentes/contador';
+import Counter from './componentes/counter';
+import Form from './componentes/nameform';
+import Items from './componentes/compItens';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Contador />
+      <Counter />
+      <Form />
       <Calculadora />
+      <Items />
     </div>
   );
 }
